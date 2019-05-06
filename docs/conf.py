342d9fd32b6143192fef30a51bf659559269ae9f
Sunaina Pai <sunainapai.in@gmail.com>
2019-05-06 21:22:48 +0530

    Remove extra comments from Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,31 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# http://www.sphinx-doc.org/en/master/config
+"""Configuration file for the Sphinx documentation builder."""
 
-# -- Path setup --------------------------------------------------------------
+# Reference: http://www.sphinx-doc.org/en/master/config
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
 
+# Add module directory to sys.path for autodoc to import the module.
+sys.path.insert(0, os.path.abspath('..'))
 
-# -- Project information -----------------------------------------------------
-
+# Project information
 project = 'MyHN'
 copyright = '2019, Sunaina Pai'
 author = 'Sunaina Pai'
 
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+# To process `automodule` directive in `.rst` files.
 extensions = [
     'sphinx.ext.autodoc',
 ]
 
-# Add any paths that contain templates here, relative to this directory.
+# Paths that contain templates, relative to this directory.
 templates_path = ['_templates']
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
+# Exclude patterns relatve to source directory.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
+# The theme to use for HTML and HTML Help pages.
 html_theme = 'alabaster'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
+# Paths that contain custom static files, relative to this directory.
 html_static_path = ['_static']
-
-
-# -- Extension configuration -------------------------------------------------
